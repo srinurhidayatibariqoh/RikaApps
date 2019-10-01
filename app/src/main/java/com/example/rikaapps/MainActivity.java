96c4eb1fc5bb311f55c5b5163a67154050abde6e
srinurhidayatibariqoh <55471773+srinurhidayatibariqoh@users.noreply.github.com>
2019-10-01 20:10:30 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rikaapps/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.rikaapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editName, editPassword;
+    TextView result;
+    Button buttonSubmit, buttonReset;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editName = (EditText) findViewById(R.id.editName);
+        editPassword = (EditText) findViewById(R.id.editPassword);
+        result = (TextView) findViewById(R.id.tvResult);
+        buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editName.getText().toString();
+                String password = editPassword.getText().toString();
+                result.setText("Name:\t" + name + "\nPassword:\t" + password);
+            }
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editName.setText("");
+                editPassword.setText("");
+                result.setText("");
+                editName.requestFocus();
+            }
+        });
+    }
+}
